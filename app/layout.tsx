f086d4cbce820dd787bb8b82cc635dbc2c994a6e
Mark Remmey <mremmey7@gmail.com>
2024-01-05 16:53:19 -0500

    Updated Messenger UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,23 @@
+import '../app/globals.css';
+
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div>
-      <h1>ChatGPT</h1>
-      {children}
-    </div>
+    <html>
+      <head />
+      <body>
+        <div className="flex">
+          {/* Sidebar */}
+
+          {/* Client Provider - Notifications */}
+
+          <div className="bg-[#343541] flex-1">{children}</div>
+        </div>
+        
+      </body>
+    </html>
   )
 }
