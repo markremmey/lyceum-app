645ba93bc7fd4010f7622b07d638123f4c16fd97
Mark Remmey <mremmey7@gmail.com>
2024-01-05 22:37:56 -0500

    Adding authentication and session management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,36 @@
 import '../app/globals.css';
+import { SessionProvider } from '../components/SessionProvider';
 import SideBar from '@/components/SideBar';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../pages/api/auth/[...nextauth]';
+import Login from '@/components/Login';
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  
+  const session = await getServerSession(authOptions)
+  
   return (
     <html>
       <head />
       <body>
-        <div className="flex">
-          {/* Sidebar */}
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]"><SideBar /></div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ): (
+          <div className="flex">
+            {/* Sidebar */}
+            <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]"><SideBar /></div>
 
-          {/* Client Provider - Notifications */}
+            {/* Client Provider - Notifications */}
 
-          <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
-        
+            <div className="bg-[#343541] flex-1">{children}</div>
+          </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   )
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: ['links.papareact.com', 'platform-lookaside.fbsbx.com', 'firebasestorage.googleapis.com']
+    },
     experimental: {
         appDir: true
     }
