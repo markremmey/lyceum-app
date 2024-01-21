0f26c6f3c765a098df69dc747f7d7f6bfea362ac
markremmey <53156818+markremmey@users.noreply.github.com>
2024-01-21 08:33:12 -0500

    Update Login.tsx

--- components/Login.tsx
+++ components/Login.tsx
@@ -12,7 +12,7 @@ function Login() {
                 height={300}
                 alt='logo'
             />
-            <button onClick={() => signIn('google')} className="text-white font-bold text-3xl animate-pulse">Sign In to use RemmeyGPT</button>
+            <button onClick={() => signIn('google')} className="text-white font-bold text-3xl animate-pulse">Sign In to use Lyceum</button>
         
         </div>
     )
