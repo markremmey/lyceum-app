4301935a485a22ec8ede6843f6159bdbcb4322e0
Mark Remmey <mremmey7@gmail.com>
2024-01-21 08:54:06 -0500

    Updates to UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { BoltIcon } from '@heroicons/react/24/outline'
 function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
-      <h1 className="text-5xl font-bold mb-20">RemmeyGPT Messenger</h1>
+      <h1 className="text-5xl font-bold mb-20">Lyceum App</h1>
 
       <div className="flex space-x-2 text-center">
         <div>
@@ -14,34 +14,6 @@ function HomePage() {
             {/*  Sun Icon */}
           
             <h2>Examples</h2>
-          </div>
-
-          <div className="space-y-2">
-            <p className="infoText">"Explain Something to me"</p>
-            <p className="infoText">"Give me some advice" </p>
-            <p className="infoText">"Who was Julius Caesar?" </p>
-          </div>
-        </div>
-
-        <div>
-          <div className="flex flex-col items-center justify-center mb-5">
-            <BoltIcon className="h-6 w-6" />
-            {/*  Sun Icon */}
-          
-            <h2>Capabilities</h2>
-          </div>
-
-          <div className="space-y-2">
-            <p className="infoText">"Explain Something to me"</p>
-            <p className="infoText">"Give me some advice" </p>
-            <p className="infoText">"Who was Julius Caesar?" </p>
-          </div>
-        </div>
-
-        <div>
-          <div className="flex flex-col items-center justify-center mb-5">
-            <ExclamationTriangleIcon className="h-6 w-6" />
-            <h2>Limitations</h2>
           </div>
 
           <div className="space-y-2">
